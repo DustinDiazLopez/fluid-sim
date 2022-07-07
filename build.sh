f689080b9099d7c5ac81edc33998080b2d13aa4a
Dustin Diaz <hunterghost526@gmail.com>
2022-07-07 18:47:51 -0500

    move pub

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 rustup target add wasm32-unknown-unknown
-
 cargo build --release
 cargo build --target wasm32-unknown-unknown --release
 cp ./target/wasm32-unknown-unknown/release/fluid_rs.wasm /public/.
