9d9bd0111a9c2ccd71366846726e587d12c6a78c
DustinDiazLopez <hunterghost526@gmail.com>
2022-08-22 05:53:09 -0500

    add link back

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # TODO: https://bevy-cheatbook.github.io/platforms/wasm.html
+
 rustup target add wasm32-unknown-unknown
 rustup target add x86_64-pc-windows-gnu
 
@@ -10,8 +11,8 @@ cargo install wasm-bindgen-cli
 trunk build
 
 trunk build --release
-rm -rf docs
-mkdir docs
+rm docs/*
+mkdir -p docs
 cp -r ./dist/* ./docs
 
-cargo build --release --target x86_64-pc-windows-gnu
+#cargo build --release --target x86_64-pc-windows-gnu
